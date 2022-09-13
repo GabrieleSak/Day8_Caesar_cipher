c8b807e7731dfa5f6acad4d42bf4ed62d1b80325
GabrieleSak <gabriele.sakalauskiene@yahoo.com>
2022-09-13 13:09:48 +0300

    Combine the encrypt() and decrypt() functions into a single function caesar()

--- main.py
+++ main.py
@@ -5,27 +5,24 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
+def caesar(text, shift, direction):
+    if direction == "encode":
+        encrypted_text = []
+        for i in range(0, len(text)):
+            index = alphabet.index(text[i])
+            encrypted_text.append(alphabet[(index + shift) % 26])
+        print(f"The encoded text is {''.join(encrypted_text)}")
+    elif direction == "decode":
+        decrypted_text = []
+        for i in range(0, len(text)):
+            index = alphabet.index(text[i])
+            decrypted_text.append(alphabet[(index - shift) % 26])
+        print(f"The decrypted text is {''.join(decrypted_text)}")
+    else:
+        print("Wrong choice")
 
-def encrypt(plain_text, encrypt_shift):
-    encrypted_text = []
-    for i in range(0, len(text)):
-        index = alphabet.index(plain_text[i])
-        encrypted_text.append(alphabet[(index + encrypt_shift) % 26])
-    print(f"The encoded text is {''.join(encrypted_text)}")
+caesar(text, shift, direction)
 
-
-def decrypt(cipher_text, decrypt_shift):
-    decrypted_text = []
-    for i in range(0, len(cipher_text)):
-        index = alphabet.index(cipher_text[i])
-        decrypted_text.append(alphabet[(index - decrypt_shift) % 26])
-    print(f"The decrypted text is {''.join(decrypted_text)}")
-
-
-if direction == "encode":
-    encrypt(text, shift)
-else:
-    decrypt(text, shift)
 
 
 
