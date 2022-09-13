8ecad12aee2840bed19b7e9a6f6f3b8c789e018a
GabrieleSak <gabriele.sakalauskiene@yahoo.com>
2022-09-13 10:32:09 +0300

    Create function 'encrypt' with 'text' and 'shift' as inputs

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
+
+
+def encrypt(text, shift):
+    encrypted_text = []
+    for i in range(0, len(text)):
+        index = alphabet.index(text[i])
+        if index + shift > 25:
+            encrypted_text.append(alphabet[index + shift - 26])
+        else:
+            encrypted_text.append(alphabet[index + shift])
+    print(encrypted_text)
+
+
+encrypt(text, shift)
+
+
