2fbe73877b3cd93cf66b4ebeedbc0dd4fa8b0214
GabrieleSak <gabriele.sakalauskiene@yahoo.com>
2022-09-13 13:49:23 +0300

    Add ASCII art logo, keep number/symbol/space at the same place, add while loop for program restart if wanted

--- main.py
+++ main.py
@@ -5,6 +5,20 @@ print(art.logo)
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
 
+
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = []
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for i in range(0, len(start_text)):
+        if start_text[i] in alphabet:
+            index = alphabet.index(start_text[i])
+            end_text.append(alphabet[(index + shift_amount) % 26])
+        else:
+            end_text.append(start_text[i])
+    print(f"The {cipher_direction}d text is {''.join(end_text)}")
+
+
 start_program = True
 
 while start_program:
@@ -13,23 +27,8 @@ while start_program:
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number:\n"))
 
-
-    def caesar(start_text, shift_amount, cipher_direction):
-        end_text = []
-        if cipher_direction == "decode":
-            shift_amount *= -1
-        for i in range(0, len(start_text)):
-            if start_text[i] in alphabet:
-                index = alphabet.index(start_text[i])
-                end_text.append(alphabet[(index + shift_amount) % 26])
-            else:
-                end_text.append(start_text[i])
-        print(f"The {cipher_direction}d text is {''.join(end_text)}")
-
-
     caesar(text, shift, direction)
 
     restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
     if restart != "yes":
         start_program = False
-
