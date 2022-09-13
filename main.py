859d77851f79338e86ad90d74d2f2d2f7658363d
GabrieleSak <gabriele.sakalauskiene@yahoo.com>
2022-09-13 11:13:05 +0300

    Create function 'decrypt', edit encrypt funtion for shifts that are larger than alphabet lenght

--- main.py
+++ main.py
@@ -6,17 +6,26 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
-def encrypt(text, shift):
+def encrypt(plain_text, encrypt_shift):
     encrypted_text = []
     for i in range(0, len(text)):
-        index = alphabet.index(text[i])
-        if index + shift > 25:
-            encrypted_text.append(alphabet[index + shift - 26])
-        else:
-            encrypted_text.append(alphabet[index + shift])
+        index = alphabet.index(plain_text[i])
+        encrypted_text.append(alphabet[(index + encrypt_shift) % 26])
     print(f"The encoded text is {''.join(encrypted_text)}")
 
 
-encrypt(text, shift)
+def decrypt(cipher_text, decrypt_shift):
+    decrypted_text = []
+    for i in range(0, len(cipher_text)):
+        index = alphabet.index(cipher_text[i])
+        decrypted_text.append(alphabet[(index - decrypt_shift) % 26])
+    print(f"The decrypted text is {''.join(decrypted_text)}")
+
+
+if direction == "encode":
+    encrypt(text, shift)
+else:
+    decrypt(text, shift)
+
 
 
